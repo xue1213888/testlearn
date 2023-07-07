f27f194ccfde25d3a8011c03797e3ef9b08b7dd2
xuesichen <xuesichen@vchangyi.com>
2023-07-07 09:54:06 +0800

    my change

--- services/users_srv/main.go
+++ services/users_srv/main.go
@@ -10,9 +10,7 @@ import (
 func main() {
 	fileName := flag.String("c", "conf/prod.yml", "config file path")
 	flag.Parse()
-
 	b, _ := os.ReadFile(*fileName)
 	fmt.Println("dev:" + string(b))
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-
 }
