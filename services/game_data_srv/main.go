fdbf49c5ccdc0c00cf11b500b8622a3c26505375
xuesichen <xsc@scccc.cn>
2023-07-08 16:40:24 +0800

    change usersrv

--- services/game_data_srv/main.go
+++ services/game_data_srv/main.go
@@ -11,6 +11,6 @@ func main() {
 	fileName := flag.String("c", "conf/prod.yml", "config file path")
 	flag.Parse()
 	b, _ := os.ReadFile(*fileName)
-	fmt.Println("users_srv:" + string(b))
+	fmt.Println("game_data_srv:" + string(b))
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 }
